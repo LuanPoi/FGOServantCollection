a9c66463a0383c349a5ae7ea5fe5002b026a1b84
LuanPoi (Ubuntu) <luan_poi@hotmail.com>
2021-01-23 15:18:25 -0300

    first commit

---
+++ app/src/main/java/com/example/fgoservantcollection/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fgoservantcollection.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fgoservantcollection.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
