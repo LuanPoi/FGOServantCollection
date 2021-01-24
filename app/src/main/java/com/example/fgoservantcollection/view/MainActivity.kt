137f206e5d010e1bbf0f77d6f4953b1e01c21590
LuanPoi (Ubuntu) <luan_poi@hotmail.com>
2021-01-24 13:36:15 -0300

    Adding Firebase Analytics to the project

--- app/src/main/java/com/example/fgoservantcollection/view/MainActivity.kt
+++ app/src/main/java/com/example/fgoservantcollection/view/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.fgoservantcollection.view
 
+import com.google.firebase.analytics.FirebaseAnalytics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fgoservantcollection.R
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
+private lateinit var firebaseAnalytics: FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics;
+        firebaseAnalytics.logEvent("EVENTO_TESTE", null);
     }
 }
